01ac788a2121e9d84bd68118ba80b3ca9e2af910
Altaf Multani <77223956+altafmultani@users.noreply.github.com>
2022-02-22 20:56:36 +0530

    FirstCommit

---
+++ NewSwiftUiProject/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  NewSwiftUiProject
+//
+//  Created by Medionce Solutions on 22/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showinglogingScreen = false
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color.blue
+                     .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack{
+                     Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("UserName", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red,width: CGFloat(wrongPassword))
+                                 
+                    Button("Login"){
+                        autheticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white )
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are Logged in @\(username)"), isActive: $showinglogingScreen) {
+                        EmptyView()
+                    }
+                }
+                
+            }
+            .navigationBarHidden(true  )
+        }
+       
+    }
+    
+    func autheticateUser(username: String, password: String) {
+        if username.lowercased() == "mario2021" {
+        wrongUsername = 0
+        if password.lowercased() == "abc123" {
+            wrongPassword = 0
+            showinglogingScreen = true
+        }else{
+            wrongPassword = 2
+        }
+        }else{
+            wrongUsername = 2
+        }
+
+          }
+         
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewSwiftUiProject/NewSwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUiProjectApp.swift
+//  NewSwiftUiProject
+//
+//  Created by Medionce Solutions on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
